838151e2abb7d490f64dec51291bb68f46bb930a
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-20 21:47:14 +0530

    Hand tracking basic

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 
 while True:
     success, img = cap.read()
+
+    # As mp can only process RGB images, convert to RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_hand_landmarks(img, handLms)
+
 
     cv2.imshow("img", img)
     cv2.waitKey(1)
