45ae5ba9d1fbfe4e8afb0aaf072dcd7ffdc8d8d9
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-06-20 23:14:57 +0530

    fps

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 
 while True:
     success, img = cap.read()
@@ -17,7 +20,17 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy =  int(lm.x*w), int(lm.y*h)
+
             mpDraw.draw_hand_landmarks(img, handLms)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
 
 
     cv2.imshow("img", img)
